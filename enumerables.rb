10e0b1893d5209460379936072bbd29698bf99b2
brandonclosson <bclosson@gmail.com>
2014-07-19 13:31:50 -0400

    All Enums Working

--- enumerables.rb
+++ enumerables.rb
@@ -61,47 +61,24 @@ module Enumerable
     count
   end
 
-  def my_map(&proc)
-    return self unless proc || block_given?
+  def my_map(&block)
+    result = []
+    self.my_each { |e| result << block.call(e) } if block_given?
+    result.empty? ? self : result
+  end
 
-    if proc
-      return self.my_each_with_index { |i, e| self[i] = proc.call(i, e) }
+  def my_inject(initial = nil)
+    result = (initial.nil? ? self.first : initial)
+    elements_needed = (initial.nil? ? (1..self.size - 1) : (0..self.size - 1))
+
+    elements_needed.to_a.each do |index|
+      result = yield(result, self[index])
     end
-    self.my_each_with_index { |i, e| self[i] = yield(e) }
+
+    result
   end
 end
 
 def multiply_elements(elements)
   elements.my_inject(1) { |sum, e| sum *= e }
 end
-
-##my_each
-my_each_result = ["hey", "hi"].my_each { |n| print n + "! " }
-print my_each_result
-print "\n"
-##my_each_with_index
-[1, 1, 2, 3, 5, 8, 13].my_each_with_index { |i, num| print num ** i; print "\t" }
-print "\n"
-##my_select
-print [1, 6, 9, 1, 2, 4, 9, 11, 15, 17, 22, 0].my_select { |n| n < 7 }
-print "\n"
-##my_all
-numbers = [1, 2, 3, 4, 5, 6, 7]
-puts numbers.my_all? { |n| n < 8 }
-puts numbers.my_all? { |n| n > 2 }
-##my_any
-puts numbers.my_any? { |n| n > 6 }
-puts numbers.my_any? { |n| n > 7 }
-##my_none?
-puts numbers.my_none? { |n| n > 7 }
-puts numbers.my_none? { |n| n < 5 }
-##my_count
-puts numbers.count
-puts numbers.count { |n| n.even? }
-puts "\n"
-##my_map
-print numbers.my_map { |n| n ** 2 }
-print "\n"
-second_power = Proc.new { |sum, n| n ** 2 }
-print [1, 2, 3, 4].my_map(&second_power)
-print "\n"
